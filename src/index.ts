03d6fcfdc6d2767d5127e5c1a5929b5a69f30da1
Z4NR34L <software@zanreal.net>
2024-01-07 18:22:18 +0100

    feat: added support for global middlewares with execution policy (before/after)

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,34 @@ import { NextResponse } from 'next/server';
 
 type MiddlewareFunction = (request: NextRequest) => Promise<NextResponse>;
 
-type Middleware = MiddlewareFunction | MiddlewareFunction[];
+type MiddlewareConfig = Record<
+  string,
+  MiddlewareFunction | MiddlewareFunction[]
+>;
 
-type MiddlewareMap = Record<string, Middleware>;
-
-export function createMiddleware(pathMiddlewareMap: MiddlewareMap) {
+export function createMiddleware(
+  pathMiddlewareMap: MiddlewareConfig,
+  globalMiddleware?: Record<string, MiddlewareFunction>,
+): MiddlewareFunction {
   return async function middleware(
     request: NextRequest,
   ): Promise<NextResponse> {
     const path = request.nextUrl.pathname || '/';
     let response: NextResponse | null = null;
 
-    for (const [key, pathMiddleware] of Object.entries(pathMiddlewareMap)) {
+    const executeGlobalMiddleware = async (type: 'before' | 'after') => {
+      const globalMiddlewareFn = globalMiddleware?.[type];
+      if (globalMiddlewareFn) {
+        const result = await executeMiddleware(request, globalMiddlewareFn);
+        if (result) response = result;
+      }
+    };
+
+    await executeGlobalMiddleware('before');
+
+    for (const [key, middlewareFunctions] of Object.entries(
+      pathMiddlewareMap,
+    )) {
       const isRegexKey = key.startsWith('regex:');
       const matchPattern = isRegexKey ? key.replace('regex:', '') : key;
 
@@ -22,51 +38,41 @@ export function createMiddleware(pathMiddlewareMap: MiddlewareMap) {
         (isRegexKey && new RegExp(matchPattern).test(path)) ||
         (!isRegexKey && pathMiddlewareMatchesPath(path, matchPattern))
       ) {
-        if (Array.isArray(pathMiddleware)) {
-          // Support multiple middleware functions in an array
-          for (const singleMiddleware of pathMiddleware) {
-            const result = await executeMiddleware(request, singleMiddleware);
-            if (result) {
-              response = result;
-            }
-          }
-        } else {
-          const result = await executeMiddleware(request, pathMiddleware);
-          if (result) {
-            response = result;
-          }
+        const middlewares = Array.isArray(middlewareFunctions)
+          ? middlewareFunctions
+          : [middlewareFunctions];
+
+        for (const middlewareFunction of middlewares) {
+          // eslint-disable-next-line no-await-in-loop -- ensuring that function fully executed
+          const result = await executeMiddleware(request, middlewareFunction);
+          if (result) response = result;
         }
       }
     }
 
-    // If no match is found, return null or a default response
+    await executeGlobalMiddleware('after');
+
     return response || NextResponse.next();
   };
 }
 
-// Helper function to execute a single middleware function
 async function executeMiddleware(
   request: NextRequest,
   middleware: MiddlewareFunction,
 ): Promise<NextResponse | null> {
   const result = await middleware(request);
-  if (result instanceof NextResponse && result !== NextResponse.next()) {
-    // Intercept before returning if a NextResponse is encountered
-    return result;
-  }
-  return result;
+  return result instanceof NextResponse && result !== NextResponse.next()
+    ? result
+    : null;
 }
 
-// Helper function to check if a pathMiddleware should match the given path
 function pathMiddlewareMatchesPath(
   path: string,
   matchPattern: string,
 ): boolean {
   if (matchPattern.includes(':path*')) {
-    // Match static paths with startsWith
     return path.startsWith(matchPattern.replace(/:path\*/, ''));
   }
-  // Match dynamic path inside [] without additional segments
   const dynamicPathRegex = new RegExp(
     `^${matchPattern.replace(/\[.*?\]/g, '([^/]+?)')}$`,
   );
