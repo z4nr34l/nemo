c5f8b1d658a2ff8691313d8f55ed3d13fbccdfea
Z4NR34L <software@zanreal.net>
2024-01-23 19:17:00 +0100

    Finished ts reconfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,17 @@
+import path from "path";
 import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   entry: ['src/**/*.ts'],
   format: ['esm', 'cjs'],
+  clean: true,
+  splitting: false,
   dts: true,
   minify: true,
-  external: [],
+  external: ['next'],
+  platform: "node",
+  target: "node20",
+  tsconfig: path.resolve(__dirname, "./tsconfig.json"),
   ...options,
 }));
