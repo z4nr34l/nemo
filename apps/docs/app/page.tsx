4c024e81d2cd681d4b64ef245f38c949e8adb295
Z4NR34L <software@zanreal.net>
2024-03-07 21:21:01 +0100

    feat: preparing docs design

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,16 +1,10 @@
-import Link from 'next/link';
+import { ReactElement } from 'react';
+import { Hero } from '@/components/hero';
 
-export default function HomePage() {
+export default function HomePage(): ReactElement {
   return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-muted-foreground">
-        You can open{' '}
-        <Link href="/docs" className="text-foreground font-semibold underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+    <main className="relative max-w-5xl mx-auto border my-[150px] bg-background">
+      <Hero />
     </main>
   );
 }
