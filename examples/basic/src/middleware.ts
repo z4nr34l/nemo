4b947acb84530711fd735ec2a3bd1dfd2c1645df
Z4NR34L <mateusz@rescale.build>
2024-07-31 09:54:35 +0200

    feat: refactored whole library, removed useless functions and simplified execution flow, minor fixes

--- examples/basic/src/middleware.ts
+++ examples/basic/src/middleware.ts
@@ -7,32 +7,32 @@ import { NextResponse } from 'next/server';
 
 const middlewares = {
   '/page1{/:path}?': [
-    async ({ request }: MiddlewareFunctionProps): Promise<NextResponse> => {
+    async ({ request }: MiddlewareFunctionProps) => {
       const response = NextResponse.next();
       console.log('Middleware for /page1', request.nextUrl.pathname);
       response.cookies.set('passed-cookie', 'cookie-value');
       response.headers.set('x-custom-header', 'header-value');
       return response;
     },
-    async ({ request }: MiddlewareFunctionProps): Promise<NextResponse> => {
+    async ({ request, response }: MiddlewareFunctionProps) => {
       console.log('Chained middleware for /page1', request.nextUrl.pathname);
       console.log('Passed cookie value:', request.cookies.get('passed-cookie'));
       console.log(
         'Passed header value:',
         request.headers.get('x-custom-header'),
       );
-      return NextResponse.next();
+      return response;
     },
   ],
   '/page2': [
-    async ({ request }: MiddlewareFunctionProps): Promise<NextResponse> => {
+    async ({ request }: MiddlewareFunctionProps) => {
       const response = NextResponse.next();
       console.log('Middleware for /page2', request.nextUrl.pathname);
       response.cookies.set('passed-cookie', 'cookie-value');
       response.headers.set('x-custom-header', 'header-value');
       return response;
     },
-    async ({ request }: MiddlewareFunctionProps): Promise<NextResponse> => {
+    async ({ request }: MiddlewareFunctionProps) => {
       const redirectUrl = 'http://localhost:3001/page1'; // Redirect within the same domain
       console.log('Redirecting to:', redirectUrl);
 
@@ -40,14 +40,14 @@ const middlewares = {
         headers: request.headers, // Transfer original headers to the redirect response
       });
     },
-    async ({ request }: MiddlewareFunctionProps): Promise<NextResponse> => {
+    async ({ request, response }: MiddlewareFunctionProps) => {
       console.log('Chained middleware for /page2', request.nextUrl.pathname);
       console.log('Passed cookie value:', request.cookies.get('passed-cookie'));
       console.log(
         'Passed header value:',
         request.headers.get('x-custom-header'),
       );
-      return NextResponse.next();
+      return response;
     },
   ],
 } satisfies MiddlewareConfig;
--- packages/next-easy-middlewares/src/index.ts
+++ packages/next-easy-middlewares/src/index.ts
@@ -8,7 +8,7 @@ export type NextMiddleware = (
 
 export interface MiddlewareFunctionProps {
   request: NextRequest;
-  response: NextResponse | Response;
+  response: NextResponse;
   context: Map<string, unknown>;
   event: NextFetchEvent;
 }
@@ -27,156 +27,83 @@ export function createMiddleware(
   globalMiddleware?: Record<string, MiddlewareFunction | MiddlewareFunction[]>,
 ): NextMiddleware {
   const context = new Map<string, unknown>();
+  let response: NextResponse = NextResponse.next();
 
   return async (
     request: NextRequest,
     event: NextFetchEvent,
   ): Promise<NextResponse | Response> => {
     const path = request.nextUrl.pathname || '/';
-    let response: NextResponse | Response = NextResponse.next();
 
-    const beforeResult = await executeGlobalMiddleware(
-      'before',
-      request,
-      response,
-      event,
-      context,
-      globalMiddleware,
-    );
-    if (beforeResult) {
-      return handleMiddlewareRedirect(request, beforeResult);
-    }
+    let beforeGlobalMiddleware: MiddlewareFunction[] = [];
+    let afterGlobalMiddleware: MiddlewareFunction[] = [];
 
-    for (const [key, middlewareFunctions] of Object.entries(
-      pathMiddlewareMap,
-    )) {
-      if (matchesPath(key, path)) {
-        response = await executePathMiddleware(
-          request,
-          response,
-          middlewareFunctions,
-          event,
-          context,
-        );
+    if (globalMiddleware?.before) {
+      if (Array.isArray(globalMiddleware.before)) {
+        beforeGlobalMiddleware = globalMiddleware.before.filter(Boolean).flat();
+      } else {
+        beforeGlobalMiddleware = [globalMiddleware.before]
+          .filter(Boolean)
+          .flat();
       }
     }
 
-    const afterResult = await executeGlobalMiddleware(
-      'after',
-      request,
-      response,
-      event,
-      context,
-      globalMiddleware,
-    );
-    if (afterResult) {
-      return handleMiddlewareRedirect(request, afterResult);
+    if (globalMiddleware?.after) {
+      if (Array.isArray(globalMiddleware.after)) {
+        afterGlobalMiddleware = globalMiddleware.after.filter(Boolean).flat();
+      } else {
+        afterGlobalMiddleware = [globalMiddleware.after].filter(Boolean).flat();
+      }
     }
 
-    return response ?? NextResponse.next();
-  };
-}
+    const allMiddlewareFunctions = [
+      ...beforeGlobalMiddleware.flat(),
+      ...Object.entries(pathMiddlewareMap)
+        .filter(([key]) => matchesPath(key, path))
+        .flatMap(([, middlewareFunctions]) => middlewareFunctions),
+      ...afterGlobalMiddleware.flat(),
+    ];
 
-async function executePathMiddleware(
-  request: NextRequest,
-  response: NextResponse | Response,
-  middlewareFunctions: MiddlewareFunction | MiddlewareFunction[],
-  event: NextFetchEvent,
-  context: Map<string, unknown>,
-): Promise<NextResponse | Response> {
-  if (!Array.isArray(middlewareFunctions)) {
-    // eslint-disable-next-line no-param-reassign -- Allow reassignment for type compatibility
-    middlewareFunctions = [middlewareFunctions];
-  }
-
-  for (const middleware of middlewareFunctions) {
-    const result = await executeMiddleware(
-      request,
-      middleware,
-      response,
-      event,
-      context,
-    );
-    if (result) {
-      response = result;
-      // eslint-disable-next-line no-param-reassign -- Allow reassignment for type compatibility
-      request = updateRequestWithResponse(request, result);
-    }
-  }
-
-  return response;
-}
-
-async function executeGlobalMiddleware(
-  type: 'before' | 'after',
-  request: NextRequest,
-  response: NextResponse | Response,
-  event: NextFetchEvent,
-  context: Map<string, unknown>,
-  globalMiddleware?: Record<string, MiddlewareFunction | MiddlewareFunction[]>,
-): Promise<NextResponse | Response> {
-  const globalMiddlewareFns = globalMiddleware?.[type];
-  if (globalMiddlewareFns) {
-    const middlewareFunctions = Array.isArray(globalMiddlewareFns)
-      ? globalMiddlewareFns
-      : [globalMiddlewareFns];
-
-    for (const middleware of middlewareFunctions) {
-      const result = await executeMiddleware(
+    for (const middleware of allMiddlewareFunctions) {
+      response = await executeMiddleware(
         request,
         middleware,
         response,
         event,
         context,
       );
-
-      if (result) {
-        if (result instanceof NextResponse) {
-          result.cookies.getAll().forEach((cookie) => {
-            request.cookies.set(cookie);
-          });
-        }
-
-        if (isRedirect(result)) {
-          request.headers.set(
-            'x-redirect-url',
-            result.headers.get('location') ?? '',
-          );
-          return result;
-        }
-
-        response = result as NextResponse;
-      }
+      request = updateRequestWithResponse(request, response);
     }
-  }
-  return NextResponse.next();
+
+    return response ?? NextResponse.next();
+  };
 }
 
 async function executeMiddleware(
   request: NextRequest,
   middleware: MiddlewareFunction,
-  currentResponse: NextResponse | Response,
+  response: NextResponse,
   event: NextFetchEvent,
   context: Map<string, unknown>,
-): Promise<NextResponse | Response> {
-  const result = await middleware({
-    request,
-    response: currentResponse,
-    event,
-    context,
-  });
+): Promise<NextResponse> {
+  const result = upgradeResponseToNextResponse(
+    await middleware({
+      request,
+      response,
+      event,
+      context,
+    }),
+  );
 
-  if (currentResponse) {
-    currentResponse.headers.forEach((value, key) => {
+  if (response) {
+    response.headers.forEach((value, key) => {
       result.headers.set(key, value);
     });
   }
 
-  if (result instanceof NextResponse) {
-    result.cookies.getAll().forEach((cookie) => {
-      request.cookies.set(cookie);
-    });
-  }
+  result.cookies.getAll().forEach((cookie) => {
+    request.cookies.set(cookie);
+  });
 
   if (isRedirect(result)) {
     request.headers.set('x-redirect-url', result.headers.get('location') ?? '');
@@ -190,27 +117,6 @@ function matchesPath(pattern: string, path: string): boolean {
   return pathToRegexp(pattern).test(path);
 }
 
-function handleMiddlewareRedirect(
-  request: NextRequest,
-  response: NextResponse | Response,
-): NextResponse | Response {
-  const redirectUrl = request.headers.get('x-redirect-url');
-
-  if (redirectUrl) {
-    const redirectResponse = NextResponse.redirect(redirectUrl, {
-      headers: request.headers,
-    });
-
-    request.cookies.getAll().forEach((cookie) => {
-      redirectResponse.cookies.set(cookie);
-    });
-
-    return redirectResponse;
-  }
-
-  return response;
-}
-
 function isRedirect(response: NextResponse | Response): boolean {
   return Boolean(
     response && [301, 302, 303, 307, 308].includes(response.status),
@@ -219,19 +125,16 @@ function isRedirect(response: NextResponse | Response): boolean {
 
 function updateRequestWithResponse(
   request: NextRequest,
-  response: NextResponse | Response,
+  response: NextResponse,
 ): NextRequest {
   const updatedHeaders = new Headers(request.headers);
 
-  // Merge headers from the response into the request headers
   response.headers.forEach((value, key) => {
     updatedHeaders.set(key, value);
   });
 
-  // Create a new URL object with the same parameters as the original request
   const updatedUrl = new URL(request.url);
 
-  // Create a new NextRequest object with the updated headers
   const updatedRequest = new NextRequest(updatedUrl, {
     headers: updatedHeaders,
     method: request.method,
@@ -239,12 +142,29 @@ function updateRequestWithResponse(
     referrer: request.referrer,
   });
 
-  // Merge cookies from the response into the request cookies
-  if (response instanceof NextResponse) {
-    response.cookies.getAll().forEach((cookie) => {
-      updatedRequest.cookies.set(cookie.name, cookie.value);
-    });
-  }
+  response.cookies.getAll().forEach((cookie) => {
+    updatedRequest.cookies.set(cookie.name, cookie.value);
+  });
 
   return updatedRequest;
+}
+
+function upgradeResponseToNextResponse(
+  response: Response | NextResponse,
+): NextResponse {
+  if (response instanceof NextResponse) {
+    return response;
+  }
+
+  const nextResponse = new NextResponse(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
+
+  response.headers.forEach((value, key) => {
+    nextResponse.headers.set(key, value);
+  });
+
+  return nextResponse;
 }
