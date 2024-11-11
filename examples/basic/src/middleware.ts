c19c3f495aebfa949372e0b0051152ab473a5aa4
Z4NR34L <mateusz@rescale.build>
2024-11-11 14:13:08 +0100

    feat: updating examples

--- examples/basic/src/middleware.ts
+++ examples/basic/src/middleware.ts
@@ -28,24 +28,27 @@ const middlewares = {
     },
   ],
   '/page2': [
-    async ({ request }: MiddlewareFunctionProps) => {
+    async ({ request, forward }: MiddlewareFunctionProps) => {
       const response = NextResponse.next();
       console.log('Middleware for /page2', request.nextUrl.pathname);
       response.cookies.set('passed-cookie', 'cookie-value');
       response.headers.set('x-custom-header', 'header-value');
-      return response;
+
+      forward(response);
     },
-    async ({ request }: MiddlewareFunctionProps) => {
+    async () => {
       const redirectUrl = 'http://localhost:3001/page1'; // Redirect within the same domain
       console.log('Redirecting to:', redirectUrl);
 
-      return NextResponse.redirect(redirectUrl, {
-        headers: request.headers, // Transfer original headers to the redirect response
-      });
+      return NextResponse.redirect(redirectUrl);
     },
+    /**
+     * THIS WILL NOT BE EXECUTED!
+     */
     async ({ request, forward }: MiddlewareFunctionProps) => {
       const response = NextResponse.next({ request });
 
+      console.log('This should not be executed');
       console.log('Chained middleware for /page2', request.nextUrl.pathname);
       console.log('Passed cookie value:', request.cookies.get('passed-cookie'));
       console.log(
