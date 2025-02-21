d68736551e517a1e15dfb207a8d65d8afb5926f0
Z4NR34L <mateusz@rescale.build>
2025-02-21 09:58:40 +0100

    chore: update global styles, add dependency for mdx-remote, and refine TypeScript imports

--- packages/nemo/src/types.ts
+++ packages/nemo/src/types.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest, NextResponse } from "next/server";
 import type { NemoEvent } from "./event";
 
 export type NextMiddlewareResult =
