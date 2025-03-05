02d3008406d9b29a2cb26f59b932932b82fcf7e0
Z4NR34L <mateusz@rescale.build>
2025-03-05 11:57:35 +0100

    feat: mark createMiddleware as deprecated and recommend using new NEMO constructor

--- packages/nemo/src/index.ts
+++ packages/nemo/src/index.ts
@@ -367,6 +367,7 @@ export class NEMO {
 }
 
 /**
+ * @deprecated Use `new NEMO()` instead. Example: `export const { middleware } = new NEMO(middlewares, globalMiddleware)`
  * Create middleware
  * @param middlewares - Middleware configuration
  * @param globalMiddleware - Global middleware configuration
@@ -375,7 +376,10 @@ export class NEMO {
 export function createMiddleware(
   middlewares: MiddlewareConfig,
   globalMiddleware?: GlobalMiddlewareConfig,
-  config?: NemoConfig,
 ) {
-  return new NEMO(middlewares, globalMiddleware, config).middleware;
+  console.warn(
+    "[NEMO] `createMiddleware` is deprecated. Use `new NEMO()` instead.",
+  );
+
+  return new NEMO(middlewares, globalMiddleware);
 }
